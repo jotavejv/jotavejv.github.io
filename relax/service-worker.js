0f3888b9e29307851052bdef9d5aa08b24bae4c1
João Victor <contatos.jv@hotmail.com>
2017-05-06 00:51:20 -0300

    improvements

--- relax/service-worker.js
+++ relax/service-worker.js
@@ -1,25 +1,26 @@
-let cacheName = 'v3';
+let cacheName = 'v2';
 let cacheFiles = [
-    'index.html',
-    'css/font-awesome.css',
-    'css/style.css',
-    'fonts/FontAwesome.otf',
-    'fonts/fontawesome-webfont.eot',
-    'fonts/fontawesome-webfont.svg',
-    'fonts/fontawesome-webfont.ttf',
-    'fonts/fontawesome-webfont.woff',
-    'fonts/fontawesome-webfont.woff2',
-    'fonts/roboto-regular-webfont.woff',
-    'fonts/roboto-regular-webfont.woff2',
-    'fonts/roboto-thin-webfont.woff',
-    'fonts/roboto-thin-webfont.woff2',
-    'app.js',
-    'images/icon-72x72.png',
-    'images/icon-96x96.png',
-    'images/icon-128x128.png',
-    'images/icon-192x192.png',
-    'images/icon-384x384.png',
-    'images/icon-512x512.png'
+    '/',
+    '/index.html',
+    '/css/font-awesome.css',
+    '/css/style.css',
+    '/fonts/FontAwesome.otf',
+    '/fonts/fontawesome-webfont.eot',
+    '/fonts/fontawesome-webfont.svg',
+    '/fonts/fontawesome-webfont.ttf',
+    '/fonts/fontawesome-webfont.woff',
+    '/fonts/fontawesome-webfont.woff2',
+    '/fonts/roboto-regular-webfont.woff',
+    '/fonts/roboto-regular-webfont.woff2',
+    '/fonts/roboto-thin-webfont.woff',
+    '/fonts/roboto-thin-webfont.woff2',
+    '/app.js',
+    '/images/icon-72x72.png',
+    '/images/icon-96x96.png',
+    '/images/icon-128x128.png',
+    '/images/icon-192x192.png',
+    '/images/icon-384x384.png',
+    '/images/icon-512x512.png'
 ]
 self.addEventListener('install', function (event){
     // cache files
