898acd838d78d804d1038b9e1f56f3b138c2a6b5
jotavejv <websolucoes.jv@gmail.com>
2018-05-20 14:46:35 -0300

    move to

--- mytasks/app.js
+++ mytasks/app.js
@@ -7,6 +7,10 @@ let allLabels = [];
 let listItems = [];
 let cardsJSON = [];
 let boardNamePrinted;
+const listTasks = "5af78e7ac91d7add92c8f78e";
+const listPriority = "5af78e97f3374a701d0cc2f9";
+const listWip = "5af78ea3ef160aa530af298a";
+const listDone = "5af78ea7b339d5de4d4eca92";
 function whichList(listID){
 	listItems = cardsJSON.filter( item => item.idList == listID);
 	//console.log(listItems);
@@ -31,6 +35,10 @@ function getLabels(boardID){
 	let api = `https://api.trello.com/1/boards/${boardID}/labels?key=${key}&token=${token}`;
 	return axios.get(api);
 }
+function moveTo(listID, cardID){
+	let api = `https://api.trello.com/1/cards/${cardID}/idList?value=${listID}&key=${key}&token=${token}`;
+	return axios.put(api);
+}
 function lists(listID){
 	let api = `https://api.trello.com/1/lists/${listID}?fields=name&key=${key}&token=${token}`;
 	return axios.get(api);
@@ -47,7 +55,8 @@ cards("5af78e6aa8d84903f0601b2c").then(res => {
 				"label": labels,
 				"idList": card.idList,
 				"color": card.labels[0].color,
-				"due": card.due
+				"due": card.due,
+				"id": card.id
 			})
 		}
 	})
@@ -58,19 +67,43 @@ cards("5af78e6aa8d84903f0601b2c").then(res => {
 })
 getLabels("5af78e6aa8d84903f0601b2c").then(res => {
 	res.data.map(item => {
-		$('select').append(`<option value="${item.name}">${item.name}</option>`);
+		$('#labels').append(`<option value="${item.name}">${item.name}</option>`);
 	});
-	$('select').on('change', function () {
+	$('#labels').on('change', function () {
 		let val = $(this).val();
 		whichLabel(val);
 	});
 })
 function render(data){
-	console.log(data);
+	//console.log(data);
 	$('.content ul').html('');
 	data.map(item => {
-		$('.content ul').append(`<li>${item.name} <span>${item.due ? formatDate(new Date(item.due)) : ''}</span> <small class="${item.color}">${item.label.join(',')}</small></li>`);
+		$('.content ul').append(`
+			<li data-id="${item.id}">${item.name}
+				<span>${item.due ? formatDate(new Date(item.due)) : ''}</span>
+				<small class="${item.color}">${item.label.join(',')}</small>
+				<i class="fa fa-ellipsis-h"></i>
+				<select class="menu">
+	    			<option value="" disabled selected>Mover para</option>
+	    			<option value="${listTasks}">Tasks</option>
+	    			<option value="${listPriority}">Priority</option>
+	    			<option value="${listWip}">WIP</option>
+	    			<option value="${listDone}">Done</option>
+	    		</select>
+			</li>
+			`);
 	})
+	$('.menu').on('change', function () {
+		let listID = $(this).val();
+		let cardID = $(this).closest('li').data('id');
+		moveTo(listID, cardID).then(res => {
+			if(res.status == 200) {
+				location.reload();
+			}else{
+				alert("Boomm", res.status)
+			}
+		});
+	});
 	bindLabelsClick();
 }
 
