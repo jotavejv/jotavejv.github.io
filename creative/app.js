ddb2fe27f4e7774dc7ad098b5fcedc0508fa3f01
jotavejv <websolucoes.jv@gmail.com>
2018-05-27 19:07:31 -0300

    pages

--- creative/app.js
+++ creative/app.js
@@ -59,19 +59,24 @@ function init(){
     window.addEventListener("resize", resizeHandler, false);
 
     //jv
+    let page;
     let $$ = document.querySelectorAll.bind(document);
     let $ = document.querySelector.bind(document);
     function closeContent(){
         $('.content').classList.remove('active');
-        //toggleMenu();
         $('nav').classList.remove('is-hidden');
+        $(`#${page}`).classList.remove('active', 'block');
     }
     Array.from($$('.link')).map(link => {
         link.addEventListener('click', function(e){
             e.preventDefault();
-            let page = e.target.textContent.toLowerCase();
+            page = e.target.textContent.toLowerCase();
             $('.content').classList.add('active');
             $('nav').classList.add('is-hidden');
+            $(`#${page}`).classList.add('block');
+            setTimeout(function() {
+                $(`#${page}`).classList.add('active');
+            }, 500);
       });
       $('.content-close').addEventListener('click', e => closeContent());
     });
