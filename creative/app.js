38ac9274a45260f6b6fa8e53dde73908823b04e3
jotavejv <websolucoes.jv@gmail.com>
2018-09-01 20:06:31 -0300

    api

--- creative/app.js
+++ creative/app.js
@@ -1,83 +1,33 @@
-function init(){
-    const elem = document.querySelector('#nav-bg'),
-          toggleBtn = document.querySelector('#toggle-btn'),
-          elemH = elem.getBoundingClientRect().height,
-          elemW = elem.getBoundingClientRect().width;
+function init() {
 
-    let open = false;
-    let scale, offsetX, offsetY;
-
-    const calculateValues = (() => {
-      const w = window.innerWidth;
-      const h = window.innerHeight;
-      //const cssStyles = getComputedStyle(elem);
-      //const offsetValue = Number(cssStyles.getPropertyValue('--offset-value'));
-      const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
-
-      //  Offsets to center the circle
-      offsetX = (w/2) - (elemW/2) - offsetValue;
-      offsetY = (h/2) - (elemH/2) - offsetValue;
-
-      // Good old pythagoras
-      const radius = Math.sqrt((Math.pow(h, 2))+(Math.pow(w, 2)));
-      scale = radius/(elemW/2)/2 + .1; // Add '.1' to compensate for Safari sub pixel blur issue
-      return scale;
-    })
-
-
-    const openMenu = () => {
-      elem.style.setProperty("--translate-x", `${offsetX}px`);
-      elem.style.setProperty("--translate-y", `-${offsetY}px`);
-      elem.style.setProperty("--scale", scale);
-    }
-    const closeMenu = () => {
-      elem.style.setProperty("--scale", 1);
-      elem.style.setProperty("--translate-x", 0);
-      elem.style.setProperty("--translate-y", 0);
-    }
-    const animateMenu = () => {
-      open ? openMenu() : closeMenu();
+    const API = "https://api.npoint.io/ddccb0fb1986eed44828";
+    const getApi = function (data) {
+        return fetch(`${API}/${data}`)
+        .then(response => response.json())
     };
 
-    const toggleMenu = () => {
-      open = !open;
-      animateMenu();
-      toggleBtn.classList.toggle('shown');
-    }
-
-    const resizeHandler = () => {
-      window.requestAnimationFrame(() => {
-        calculateValues();
-        animateMenu();
-      });
-    }
-
-    calculateValues();
-
-    //toggleBtn.onclick = toggleMenu;
-    toggleBtn.addEventListener('click', toggleMenu, false);
-    window.addEventListener("resize", resizeHandler, false);
-
-    //jv
     let page;
     let $$ = document.querySelectorAll.bind(document);
     let $ = document.querySelector.bind(document);
-    function closeContent(){
+
+    function closeContent() {
         $('.content').classList.remove('active');
         $('nav').classList.remove('is-hidden');
         $(`#${page}`).classList.remove('active', 'block');
     }
     Array.from($$('.link')).map(link => {
-        link.addEventListener('click', function(e){
+        link.addEventListener('click', function (e) {
             e.preventDefault();
             page = e.target.textContent.toLowerCase();
             $('.content').classList.add('active');
             $('nav').classList.add('is-hidden');
             $(`#${page}`).classList.add('block');
-            setTimeout(function() {
+            setTimeout(function () {
                 $(`#${page}`).classList.add('active');
             }, 500);
-      });
+        });
     });
     $('.content-close').addEventListener('click', e => closeContent());
+
+    getApi("labs").then(data => console.log(data[0]));
 }
--- creative/service-worker.js
+++ creative/service-worker.js
@@ -1,5 +1,84 @@
-// let cacheName = 'v6';
-// let cacheFiles = [
+// // let cacheName = 'v6';
+// // let cacheFiles = [
+// //     '/mytasks/',
+// //     '/mytasks/index.html',
+// //     '/mytasks/css/font-awesome.css',
+// //     '/mytasks/css/style.css',
+// //     '/mytasks/fonts/FontAwesome.otf',
+// //     '/mytasks/fonts/fontawesome-webfont.eot',
+// //     '/mytasks/fonts/fontawesome-webfont.svg',
+// //     '/mytasks/fonts/fontawesome-webfont.ttf',
+// //     '/mytasks/fonts/fontawesome-webfont.woff',
+// //     '/mytasks/fonts/fontawesome-webfont.woff2',
+// //     '/mytasks/fonts/roboto-regular-webfont.woff',
+// //     '/mytasks/fonts/roboto-regular-webfont.woff2',
+// //     '/mytasks/fonts/roboto-thin-webfont.woff',
+// //     '/mytasks/fonts/roboto-thin-webfont.woff2',
+// //     '/mytasks/app.js',
+// //     '/mytasks/images/icons/icon-72x72.png',
+// //     '/mytasks/images/icons/icon-96x96.png',
+// //     '/mytasks/images/icons/icon-128x128.png',
+// //     '/mytasks/images/icons/icon-192x192.png',
+// //     '/mytasks/images/icons/icon-384x384.png',
+// //     '/mytasks/images/icons/icon-512x512.png'
+// // ]
+// // self.addEventListener('install', function (event){
+// //     // cache files
+// //     event.waitUntil(
+// //         caches.open(cacheName).then(function (cache){
+// //             return cache.addAll(cacheFiles)
+// //         })
+// //     )
+// // });
+// //
+// // self.addEventListener("activate", function(event) {
+// //     /* Just like with the install event, event.waitUntil blocks activate on a promise.
+// //      Activation will fail unless the promise is fulfilled.
+// //      */
+// //     console.log('WORKER: activate event in progress.');
+// //
+// //     event.waitUntil(
+// //         caches
+// //         /* This method returns a promise which will resolve to an array of available
+// //          cache keys.
+// //          */
+// //             .keys()
+// //             .then(function (keys) {
+// //                 // We return a promise that settles when all outdated caches are deleted.
+// //                 return Promise.all(
+// //                     keys
+// //                         .filter(function (key) {
+// //                             // Filter by keys that don't start with the latest version prefix.
+// //                             return !key.startsWith(version);
+// //                         })
+// //                         .map(function (key) {
+// //                             /* Return a promise that's fulfilled
+// //                              when each outdated cache is deleted.
+// //                              */
+// //                             return caches.delete(key);
+// //                         })
+// //                 );
+// //             })
+// //             .then(function() {
+// //                 console.log('WORKER: activate completed.');
+// //             })
+// //     );
+// // });
+// //
+// // // fetch pattern
+// // self.addEventListener('fetch', function(event) {
+// //     //console.log(event.request);
+// //     event.respondWith(
+// //         caches.match(event.request).then(function(cachedResponse) {
+// //             return cachedResponse || fetch(event.request);
+// //         })
+// //     );
+// // });
+// //
+// // //This is the service worker with the Cache-first network
+
+// var CACHE = 'pwabuilder-precache2';
+// var precacheFiles = [
 //     '/mytasks/',
 //     '/mytasks/index.html',
 //     '/mytasks/css/font-awesome.css',
@@ -21,141 +100,62 @@
 //     '/mytasks/images/icons/icon-192x192.png',
 //     '/mytasks/images/icons/icon-384x384.png',
 //     '/mytasks/images/icons/icon-512x512.png'
-// ]
-// self.addEventListener('install', function (event){
-//     // cache files
-//     event.waitUntil(
-//         caches.open(cacheName).then(function (cache){
-//             return cache.addAll(cacheFiles)
-//         })
-//     )
+// ];
+
+// //Install stage sets up the cache-array to configure pre-cache content
+// self.addEventListener('install', function(evt) {
+//   console.log('The service worker is being installed.');
+//   evt.waitUntil(precache().then(function() {
+//     console.log('[ServiceWorker] Skip waiting on install');
+//       return self.skipWaiting();
+
+//   })
+//   );
 // });
-//
-// self.addEventListener("activate", function(event) {
-//     /* Just like with the install event, event.waitUntil blocks activate on a promise.
-//      Activation will fail unless the promise is fulfilled.
-//      */
-//     console.log('WORKER: activate event in progress.');
-//
-//     event.waitUntil(
-//         caches
-//         /* This method returns a promise which will resolve to an array of available
-//          cache keys.
-//          */
-//             .keys()
-//             .then(function (keys) {
-//                 // We return a promise that settles when all outdated caches are deleted.
-//                 return Promise.all(
-//                     keys
-//                         .filter(function (key) {
-//                             // Filter by keys that don't start with the latest version prefix.
-//                             return !key.startsWith(version);
-//                         })
-//                         .map(function (key) {
-//                             /* Return a promise that's fulfilled
-//                              when each outdated cache is deleted.
-//                              */
-//                             return caches.delete(key);
-//                         })
-//                 );
-//             })
-//             .then(function() {
-//                 console.log('WORKER: activate completed.');
-//             })
-//     );
+
+
+// //allow sw to control of current page
+// self.addEventListener('activate', function(event) {
+// console.log('[ServiceWorker] Claiming clients for current page');
+//       return self.clients.claim();
+
 // });
-//
-// // fetch pattern
-// self.addEventListener('fetch', function(event) {
-//     //console.log(event.request);
-//     event.respondWith(
-//         caches.match(event.request).then(function(cachedResponse) {
-//             return cachedResponse || fetch(event.request);
-//         })
-//     );
+
+// self.addEventListener('fetch', function(evt) {
+//   console.log('The service worker is serving the asset.'+ evt.request.url);
+//   evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+//   evt.waitUntil(update(evt.request));
 // });
-//
-// //This is the service worker with the Cache-first network
-
-var CACHE = 'pwabuilder-precache2';
-var precacheFiles = [
-    '/mytasks/',
-    '/mytasks/index.html',
-    '/mytasks/css/font-awesome.css',
-    '/mytasks/css/style.css',
-    '/mytasks/fonts/FontAwesome.otf',
-    '/mytasks/fonts/fontawesome-webfont.eot',
-    '/mytasks/fonts/fontawesome-webfont.svg',
-    '/mytasks/fonts/fontawesome-webfont.ttf',
-    '/mytasks/fonts/fontawesome-webfont.woff',
-    '/mytasks/fonts/fontawesome-webfont.woff2',
-    '/mytasks/fonts/roboto-regular-webfont.woff',
-    '/mytasks/fonts/roboto-regular-webfont.woff2',
-    '/mytasks/fonts/roboto-thin-webfont.woff',
-    '/mytasks/fonts/roboto-thin-webfont.woff2',
-    '/mytasks/app.js',
-    '/mytasks/images/icons/icon-72x72.png',
-    '/mytasks/images/icons/icon-96x96.png',
-    '/mytasks/images/icons/icon-128x128.png',
-    '/mytasks/images/icons/icon-192x192.png',
-    '/mytasks/images/icons/icon-384x384.png',
-    '/mytasks/images/icons/icon-512x512.png'
-];
-
-//Install stage sets up the cache-array to configure pre-cache content
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('[ServiceWorker] Skip waiting on install');
-      return self.skipWaiting();
-
-  })
-  );
-});
 
 
-//allow sw to control of current page
-self.addEventListener('activate', function(event) {
-console.log('[ServiceWorker] Claiming clients for current page');
-      return self.clients.claim();
-
-});
-
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.'+ evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
-  evt.waitUntil(update(evt.request));
-});
+// function precache() {
+//   return caches.open(CACHE).then(function (cache) {
+//     return cache.addAll(precacheFiles);
+//   });
+// }
 
 
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(precacheFiles);
-  });
-}
+// function fromCache(request) {
+//   //we pull files from the cache first thing so we can show them fast
+//   return caches.open(CACHE).then(function (cache) {
+//     return cache.match(request).then(function (matching) {
+//       return matching || Promise.reject('no-match');
+//     });
+//   });
+// }
 
 
-function fromCache(request) {
-  //we pull files from the cache first thing so we can show them fast
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
+// function update(request) {
+//   //this is where we call the server to get the newest version of the
+//   //file to use the next time we show view
+//   return caches.open(CACHE).then(function (cache) {
+//     return fetch(request).then(function (response) {
+//       return cache.put(request, response);
+//     });
+//   });
+// }
 
-
-function update(request) {
-  //this is where we call the server to get the newest version of the
-  //file to use the next time we show view
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
-
-function fromServer(request){
-  //this is the fallback if it is not in the cahche to go to the server and get it
-return fetch(request).then(function(response){ return response})
-}
+// function fromServer(request){
+//   //this is the fallback if it is not in the cahche to go to the server and get it
+// return fetch(request).then(function(response){ return response})
+// }
